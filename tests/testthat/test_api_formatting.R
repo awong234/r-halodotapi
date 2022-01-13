c19a37824528d2ae3f0f8104b63f196281b36abd
Alec Wong <aw685@cornell.edu>
2022-01-12 22:38:34 -0500

    update one test

--- tests/testthat/test_api_formatting.R
+++ tests/testthat/test_api_formatting.R
@@ -23,21 +23,16 @@ test_that('CSR api formats correctly', {
 
 test_that('Match API formats correctly', {
     test_gamertag = paste0(sample(LETTERS, 5), collapse = '')
-    test_page = rpois(n = 1, lambda = 1)
-    res = api_player_matches(gamertag = test_gamertag, page = test_page)
-    ressplit = strsplit(res, split = '/')[[1]]
-    test_components = c(
-        'https:',
-        '',
-        'cryptum.halodotapi.com',
-        'games',
-        'hi',
-        'stats',
-        'players',
-        test_gamertag,
-        glue::glue('matches?page={test_page}')
-    )
-    expect_true(all.equal(ressplit, test_components))
+    res = api_player_list_matches(gamertag = test_gamertag, count = 1, offset = 0)
+    expect_s3_class(res, 'httr2_request')
+    url = res$url
+    split_url = strsplit(url, split = '/')[[1]]
+    query = gsub(pattern = "\\?", replacement = "", x = split_url[8])
+    split_query = strsplit(query, split = '&')[[1]]
+    split_query = strsplit(split_query, split = '=')
+    expect_equal(split_query[[1]][2], test_gamertag)
+    expect_equal(split_query[[2]][2], '1')
+    expect_equal(split_query[[3]][2], '0')
 })
 
 test_that('Service record API formats correctly', {
